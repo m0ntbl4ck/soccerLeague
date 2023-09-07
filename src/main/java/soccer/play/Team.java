2d1d460513648c7c9718c43bedac6a6842f6985b
m0ntbl4ck <mont.black.123@gmail.com>
2023-09-06 23:38:35 -0500

    CREADED CLASS PLAYER, PLAYERDATABASE, SETTINGS Y GAMEEVENT

---
+++ src/main/java/soccer/play/Team.java
@@ -0,0 +1,16 @@
+
+package soccer.play;
+
+/**
+ *
+ * @author mont_
+ */
+public class Team {
+    private String teamName;
+    private Player[] playerArray;
+    private int pointsTotal;
+    private int goalsTotal;
+    private boolean detailAvailable = false;
+    private int id = 0;
+    private String detailType = "Team";
+}
