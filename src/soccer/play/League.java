d4e7501207ad94205ad09a7e1b3d603cbf58b026
m0ntbl4ck <mont.black.123@gmail.com>
2023-09-07 15:10:56 -0500

    CHANGE FROM LIBRARY TO ANT

---
+++ src/soccer/play/League.java
@@ -0,0 +1,17 @@
+
+package soccer.play;
+
+/**
+ *
+ * @author mont_
+ */
+public class League {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
